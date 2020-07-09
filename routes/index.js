9cc2314a77474c665a7eee9b0c2ee74996ff3342
370455671 <370455671@qq.com>
2020-07-10 02:08:59 +0800

    feat add bligList.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,36 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-10 01:17:54
+ * @LastEditTime: 2020-07-10 01:59:09
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \node\koa2_weibo\routes\index.js
+ */
+
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!',
-    msg: "你好",
-    isMe: false
+    isMe: true,
+    blogListDataSource: [
+      {
+        id: 1,
+        title: "111"
+      },
+      {
+        id: 2,
+        title: "222"
+      },
+      {
+        id: 3,
+        title: "333"
+      },
+      {
+        id: 4,
+        title: "444"
+      },
+    ]
   })
 })
 
